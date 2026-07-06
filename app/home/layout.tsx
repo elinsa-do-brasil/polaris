da7dccd682ac73b0b56bdd6577cc02cfb2bcf0eb
Raave Aires <developer@raave.me>
2026-07-06 15:38:16 -0300

    fix: adiciona provider no layout home

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,9 +1,13 @@
 import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { i18nProvider } from "fumadocs-ui/i18n";
+import { RootProvider } from "fumadocs-ui/provider/next";
 import { i18n } from "@/lib/i18n";
-import { baseOptions } from "@/lib/layout.shared";
+import { baseOptions, translations } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/home">) {
   return (
-    <HomeLayout {...baseOptions(i18n.defaultLanguage)}>{children}</HomeLayout>
+    <RootProvider i18n={i18nProvider(translations, i18n.defaultLanguage)}>
+      <HomeLayout {...baseOptions(i18n.defaultLanguage)}>{children}</HomeLayout>
+    </RootProvider>
   );
 }
