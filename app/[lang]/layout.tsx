c981851cd6fe7a0eb8d0ec8d83807a1c479e895a
Raave Aires <developer@raave.me>
2026-07-11 09:45:05 -0300

    feat: adiciona ícones de favicon ao layout

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,8 +2,8 @@ import { i18nProvider } from "fumadocs-ui/i18n";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import type { Metadata } from "next";
 import "@/app/global.css";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { Inter } from "next/font/google";
 import { baseOptions, translations } from "@/lib/layout.shared";
@@ -17,6 +17,24 @@ export const metadata: Metadata = {
   title: {
     template: "%s | Elinsa",
     default: "Docs | Elinsa",
+  },
+  icons: {
+    icon: [
+      {
+        url: "/favicon/e.svg",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon/e.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon/e.ico",
+      },
+    ],
+    shortcut: "/favicon/e.ico",
+    apple: "/favicon/e.png",
   },
 };
 
