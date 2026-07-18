26f3be0b0e7899953e8e32e8bab844cb8e19591e
Raave Aires <developer@raave.me>
2026-07-18 11:34:01 -0300

    feat: adiciona configuração de formatos de imagem no Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  }
 };
 
 export default withMDX(config);
