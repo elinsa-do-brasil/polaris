cb1701b0e6016268270afe98983a89a3a3616f61
Raave Aires <developer@raave.me>
2026-07-18 11:37:26 -0300

    feat: adiciona e implementa o componente ImageZoom

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,11 +1,20 @@
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { cn } from "@/lib/cn";
 import { DownloadableFile } from "./downloadable-file";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
     DownloadableFile,
+    img: (props) => (
+      <ImageZoom
+        {...(props as any)}
+        className={cn("rounded-lg", props.className)}
+        zoomInProps={{ className: "rounded-lg" }}
+      />
+    ),
     ...components,
   } satisfies MDXComponents;
 }
